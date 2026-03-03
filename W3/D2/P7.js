e42c4cefc48ac01ca4746475202b2677bd802274
bindushrees-web <bindushrees754@gmail.com>
2026-03-03 21:01:39 +0530

    Added arrow function

---
+++ W3/D2/P7.js
@@ -0,0 +1,17 @@
+// Arrow Function
+(args) => {
+    //body of the function
+}
+
+//Add two numbers
+const Sum = (a,b) => {
+    return a+b;
+}
+console.log("3+3 =",Sum(3,3))
+
+//Single line return / implicit return
+const square = x => x*x;
+console.log("Square of 44:",square(44));
+
+const sayHello = () => console.log("Hello from arrow func");
+sayHello();
