70d81eba9e916486815f6a5d87b35642ae21501a
bindushrees-web <bindushrees754@gmail.com>
2026-03-03 20:57:33 +0530

    Added Comparision Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+// Comparision Operators
+
+console.log("Coparision operator");
+let x=2, y='4';
+console.log("x==y: ",x==y); //Loose equality
+console.log("x===y: ",x===y); //Strict equality
+console.log("x!=y: ",x!=y); //Loose inequality
+console.log("x!==y: ",x!==y); //Strict inequality
+console.log("x>3: ",x>3); 
+console.log("x<3: ",x<3); 
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);  
