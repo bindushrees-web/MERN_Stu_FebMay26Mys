766b01b8c28a3ecd26571761114456620b53b73e
bindushrees-web <bindushrees754@gmail.com>
2026-03-03 20:56:41 +0530

    Added arithmetic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,15 @@
+// Arithmetic Operators
+
+console.log("Arithmetic Operators");
+const a=2, b=2;
+let c=0;
+console.log("a+b:",a+b);
+console.log("a-b:",a-b);
+console.log("a*b:",a*b);
+console.log("a/b:",a/b);
+console.log("a%b:",a%b); //Modulus
+console.log("a**b:",a**b); //Exponential
+console.log("a++:",a++); //Post-increment
+console.log("++a:",++a); //Pre-incrementṇ
+console.log("a--:",a--); //Post-decrement
+console.log("--a:",--a); //Pre-decrement
