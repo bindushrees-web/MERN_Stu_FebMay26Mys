498653a4ab4c19b7defb0a17fd72680a6bf5615b
bindushrees-web <bindushrees754@gmail.com>
2026-03-03 21:02:54 +0530

    Added IIFE

---
+++ W3/D2/P9.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expression(IIFE)
+
+// Without Parameters
+(function(){
+    console.log("Basic IIFE executed Immediately");
+})();
+
+//With Parameters
+(function(appName,version){
+    console.log("App:",appName,"Version:",version);
+})("NodeJs","V22.22.0");
+
+//With return value
+const result = (function(){
+    const a = 10, b = 20;
+    return a+b;
+})();
+console.log("Sum is",result);
