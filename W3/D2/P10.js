70fdb5ddcd1f517745aa659878dd2bce07579761
bindushrees-web <bindushrees754@gmail.com>
2026-03-03 21:03:28 +0530

    Added callback function

---
+++ W3/D2/P10.js
@@ -0,0 +1,11 @@
+// Callback Function
+
+// Is a function which is passed as an argument as an argument to another function
+
+function greetUser(name, callback){
+    console.log("Hello, ",name);
+    callback();
+}
+greetUser("Bindu",function(){
+    console.log("Callback function executed");
+});
