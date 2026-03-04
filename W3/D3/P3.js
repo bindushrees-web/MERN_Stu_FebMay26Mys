bc1104c326b47631607e3077260011991bfeefe3
bindushrees-web <bindushrees754@gmail.com>
2026-03-04 13:38:56 +0530

    Added break and continue

---
+++ W3/D3/P3.js
@@ -0,0 +1,17 @@
+// Break 
+
+for(let i=0;i<4;i++){  
+    if(i===2){
+         console.log("i=",i); 
+        break;
+    }
+}
+
+//Continue
+
+for(let i=0;i<5;i++){  
+    if(i===4){
+         console.log("i=",i); 
+        continue;
+    }
+}
