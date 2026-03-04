d2d0d5b43c82fc0bc9d7977fcf18ca0d4386b2a4
bindushrees-web <bindushrees754@gmail.com>
2026-03-04 13:29:46 +0530

    Added while and do-while loops

---
+++ W3/D3/P2.js
@@ -0,0 +1,17 @@
+// While loop
+
+console.log("while loop examples");
+
+let count = 0;
+while(count < 5){
+    console.log("count=",count);
+    count++;
+}
+
+// Do While loop
+
+let value = 0;
+do{
+    console.log("Value :",value);
+    value++;
+}while(value < 3);
