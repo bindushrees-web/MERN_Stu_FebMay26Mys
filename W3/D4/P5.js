0ff8d7fa665f264cd23e989c250e109f20386c1d
bindushrees-web <bindushrees754@gmail.com>
2026-03-05 17:18:18 +0530

    Added reduced  method

---
+++ W3/D4/P5.js
@@ -0,0 +1,15 @@
+//Reduce mathod
+
+let numbers = [2,4,6,8,10];
+let sum = numbers.reduce((intermediateSum,current)=>intermediateSum+current,0);
+console.log(sum);
+let avg = numbers.reduce((intermediateSum,current)=>intermediateSum+current,0)/numbers.length;
+console.log(avg);
+
+//Reduce to object count by category
+let items = ["book","pen","pencil","eraser","pen"];
+let count = items.reduce((intermediateVal,item)=>{
+    intermediateVal[item] = (intermediateVal[item] || 0) + 1;
+    return intermediateVal;
+},{});
+console.log("Item count:",count);
