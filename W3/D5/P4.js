eb64462725c98d194fae86acc04dabbf620d18ff
bindushrees-web <bindushrees754@gmail.com>
2026-03-06 21:36:54 +0530

    Added Keyboard event: keydown

---
+++ W3/D5/P4.js
@@ -0,0 +1,8 @@
+//Keyboard event: keydown
+const input = document.getElementById("nameInput");
+const display = document.getElementById("display");
+
+input.addEventListener("keydown", function(event){
+    console.log("Key pressed:",event.key);
+    display.textContent = "Last key pressed: "+event.key;
+});
