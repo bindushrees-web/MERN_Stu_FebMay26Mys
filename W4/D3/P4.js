a8600a37deb4de592ffb24068d77c26ba96d4035
bindushrees-web <bindushrees754@gmail.com>
2026-03-11 12:37:36 +0530

    Added Storing and retreiving JSON data

---
+++ W4/D3/P4.js
@@ -0,0 +1,21 @@
+//Storing and retreiving JSON data
+
+const jsonOutput = document.getElementById("jsonOutput");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    const user = {
+        Id:101,
+        Name:"Bindu",
+        role:"Software developer",
+        skills:["html","CSS","JS"]
+    };
+    localStorage.setItem("userProfile",JSON.stringify(user));
+    jsonOutput.textContent = "user object saved as string to localStorage";
+});
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const up = localStorage.getItem("userProfile");
+    console.log(JSON.parse(up));
+    console.log(up);
+    jsonOutput.textContent = "userProfile" + up;
+});
