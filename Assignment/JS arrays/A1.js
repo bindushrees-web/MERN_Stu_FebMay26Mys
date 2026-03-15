16b27906c6b3127d9bc5a01710e3947214934dad
bindushrees-web <bindushrees754@gmail.com>
2026-03-15 22:04:14 +0530

    Support ticket queue processor

---
+++ Assignment/JS arrays/A1.js
@@ -0,0 +1,27 @@
+//Support ticket queue processor
+let ticketQueue=[
+    {id:"T101",priority:"HIGH",resolved:true},
+    {id:"T102",priority:"LOW",resolved:true},
+    {id:"T103",priority:"MEDIUM",resolved:false},
+    {id:"T104",priority:"LOW",resolved:false},
+    {id:"T105",priority:"HIGH",resolved:false}
+];
+console.log("ticketQueue:",ticketQueue);
+
+ticketQueue.unshift({id:"T100",priority:"MEDIUM",resolved:true});
+
+ticketQueue.push({id:"T106",priority:"MEDIUM",resolved:true},
+                {id:"T107",priority:"HIGH",resolved:true});
+
+const currentTicket = ticketQueue.shift();
+
+const droppedTicket = ticketQueue.pop();
+
+let  pending = ticketQueue.filter(ticket => !ticket.resolved);
+
+let pendingIds = pending.map(tickets => tickets.id);
+
+console.log("Current Tickets:",currentTicket);
+console.log("Dropped Tickets:",droppedTicket);
+console.log("Pending Tickets:",pending);
+console.log("Pending Ids:",pendingIds);
