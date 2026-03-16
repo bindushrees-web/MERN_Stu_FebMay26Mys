373f1564a77e9d0ca4589d62246ffd190a19a6a1
bindushrees-web <bindushrees754@gmail.com>
2026-03-16 16:27:05 +0530

    Array destruction

---
+++ W5/D1/P2.js
@@ -0,0 +1,10 @@
+//Array destruction
+const items = {
+    item1: "Laptop",
+    item2: "Server",
+    item3: "CloudAPI"
+};
+
+for(const [item,itemName] of Object.entries(items)){
+    console.log(`${item}:${itemName}`);
+}
