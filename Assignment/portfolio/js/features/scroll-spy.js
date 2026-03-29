6205122e9dd52109a98358a3a794434e970c3cfa
bindushrees-web <bindushrees754@gmail.com>
2026-03-29 23:53:59 +0530

    scroll spy

---
+++ Assignment/portfolio/js/features/scroll-spy.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("text-blue-600");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("text-blue-600");
+        }
+    });
+});
