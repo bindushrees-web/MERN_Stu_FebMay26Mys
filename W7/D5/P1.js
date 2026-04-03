aacc1ac6b8049de4fba37cd1a467ee991dcdb74d
bindushrees-web <bindushrees754@gmail.com>
2026-04-03 10:04:44 +0530

    How Cookie is used to track session ID

---
+++ W7/D5/P1.js
@@ -0,0 +1,9 @@
+// How Cookie is used to track session ID
+
+//Simulated server-side session store
+const sessionStore = { "abc123": { userId: 101, username: "Bindu", role: "student"}};
+
+//Simulated browser cookie value
+const browserCookieSessionId = "abc123";
+const sessionData = sessionStore[browserCookieSessionId];
+console.log("Server-side session data:", sessionData);
