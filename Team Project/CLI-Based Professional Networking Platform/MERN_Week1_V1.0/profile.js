1d954338aa35ce3eaeceb3483b190d72c3694040
bindushrees-web <bindushrees754@gmail.com>
2026-04-16 10:59:06 +0530

    LinkedIn Clone Project

--- Team Project/CLI-Based Professional Networking Platform/MERN_Week1_V1.0/profile.js
+++ Team Project/CLI-Based Professional Networking Platform/MERN_Week1_V1.0/profile.js
@@ -1,47 +1,47 @@
+const chalk = require("chalk");
 const user = require("./user");
-function editProfile(r1,callback){
-    const CurrentUser = user.getCurrentUser();
-    if(!CurrentUser){
-        console.log("\n Create profile First\n");
+
+function editProfile(rl,callback) {
+    const currentUser = user.getcurrentUser();
+
+    if(!currentUser) {
+        console.log("\nCreate Profile First\n");
         return callback();
-        
     }
+
     console.log("\n================================\n");
     console.log("1. Add Skill");
     console.log("2. Add Experience");
-    console.log("3. Add education");
-    r1.question("enter your choice :-",(choice)=>{
-        switch(choice){
+    console.log("3. Add Education");
+    rl.question("Enter your choice :",(choice)=> {
+        switch(choice) {
             case "1":
-                r1.question("enter your skill",(skill)=>{
-                    CurrentUser.skills.push(skill);
-                    console.log("skill added sucessfully");
+                rl.question("Enter Your skill: ",(skill)=>{
+                    currentUser.skills.push(skill);
+                    console.log("Skill added sucessfully");
                     callback();
                     
                 });
                 break;
             case "3":
-                r1.question("enter your education",(education)=>{
-                    CurrentUser.education.push(education);
-                    console.log("education added sucessfully");
+                rl.question("Enter Your Education:  ",(education)=>{
+                    currentUser.education.push(education);
+                    console.log("Education added sucessfully");
                     callback();
                     
                 });
                 break;
             case "2":
-                r1.question("enter your education",(experience)=>{
-                    CurrentUser.experience.push(experience);
-                    console.log("education added sucessfully");
+                rl.question("Enter Your Experience: ",(experience)=>{
+                    currentUser.experience.push(experience);
+                    console.log("Experience added sucessfully");
                     callback();
                 });
                 break; 
             default:
-                console.log("\ninvalid chice\n");
-                callback();
-                  
+                console.log("\nInvalid choice\n");
+                callback();     
         }
     });
 }
-module.exports = {
-    editProfile
-};
+module.exports = { editProfile };
