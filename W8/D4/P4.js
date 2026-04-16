e7b87cba8f934158b350b617bba2efcd017abe63
bindushrees-web <bindushrees754@gmail.com>
2026-04-16 11:50:26 +0530

    OTP generation using Crypto build-in module of NodeJS

---
+++ W8/D4/P4.js
@@ -0,0 +1,8 @@
+// OTP generation using Crypto build-in module of NodeJS
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+
+function generateOTP(length = 6) {
+    return crypto.randomInt(0,10**length).toString().padStart(length,"0");
+}
+console.log("OTP: ",generateOTP());
