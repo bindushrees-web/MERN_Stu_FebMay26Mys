4ac3e6948d3328e5a5cf29213ee5d06a55280233
bindushrees-web <bindushrees754@gmail.com>
2026-04-22 13:09:36 +0530

    Created auth middleware in which verified token and fetched user details

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const user = require("../models/User");
 const User = require("../models/User");
 
 // Auth middleware
@@ -7,17 +6,17 @@ exports.protect = async(req, res, next) => {
     try {
         let token;
         if (
-            req.headers.authorization.authorization &&
+            req.headers.authorization &&
             req.headers.authorization.startsWith("Bearer")
         ){
-            token = req.authorization.split(" ")[1];
+            token = req.headers.authorization.split(" ")[1];
         }
         if(!token) {
             return res.status(401).json({
                 success: false,
                 message: "Not authorized, token missing",
             });
-
+        }
             // Verify token
             const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
@@ -34,7 +33,6 @@ exports.protect = async(req, res, next) => {
             // Attach user to request
             req.user = user;
             next();
-        }
     }
     catch(error) {
          return res.status(401).json({
