c10ed75c0bd1a0e62843b9d998b495bc17a1a221
bindushrees-web <bindushrees754@gmail.com>
2026-04-28 08:50:21 +0530

    modified

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/movie.service.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/movie.service.js
@@ -1,4 +1,5 @@
 const Movie = require("../models/Movie");
+const CustomError = require("../utils/customError");
 
 // Create Movie
 exports.createMovie = async (data) => {
@@ -57,7 +58,7 @@ exports.updateMovie = async (id, data) => {
     });
 
     if (!movie)
-        throw new Error("Movie not found");
+        throw new CustomError("Movie not found",404);
     return movie;
 };
 
@@ -68,5 +69,5 @@ exports.deleteMovie = async (id) => {
         isActive: false,
     });
     if (!movie)
-        throw new Error("Movie not found");
+        throw new CustomError("Movie not found",404);
 };
