a5da9e6533e513b225010c7f6ac82d80e013bc60
bindushrees-web <bindushrees754@gmail.com>
2026-04-29 11:48:01 +0530

    State Lifting

--- W9/D2/prop-example-app/src/components/P5.jsx
+++ W9/D2/prop-example-app/src/components/P5.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
+import {useState} from 'react';
 // State Lifting: moving shared state up to the closest common parent
 // We use this: 
 // Multiple child components need to share the same data
