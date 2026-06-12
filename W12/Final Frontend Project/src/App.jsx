53357d86fbcb09df70dc0070292bbe212b04e58b
bindushrees-web <bindushrees754@gmail.com>
2026-06-12 12:04:31 +0530

    Usage of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-import HomePage from './pages/HomePage';
+import MovieCard from './components/MovieCard';
 
 function App() {
 
   return (
-      // Component Composition
-      <HomePage />
-  )
+      <>
+      <MovieCard title="Inception" genre="Sci-Fi" rating={4.8} duration="2h 28m" poster="https://picsum.photos/200/300?1" />
+      <MovieCard title="Avatar" genre="Sci-Fi" rating={4.1} duration="2h 28m" poster="https://picsum.photos/200/300?2" />
+      <MovieCard title="FightClub" genre="Sci-Fi" rating={4.9} duration="2h 28m" poster="https://picsum.photos/200/300?3" />
+      </>
+  );
 }
 
 export default App
