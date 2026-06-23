14d15adeab4576c724b89bd0663c04e1a22e7a0a
bindushrees-web <bindushrees754@gmail.com>
2026-06-23 09:15:05 +0530

    APIs for Movie CRUD and Show CRUD operation

--- W12/bookmyshow-frontend/src/api/show.api.js
+++ W12/bookmyshow-frontend/src/api/show.api.js
@@ -29,14 +29,93 @@ import api from "./axios";
 GET SHOWS
 
 
-GET /api/shows?movieId=:movieId
+/*
+=========================================================
+GET SHOW BY ID
+
+
+GET /api/shows/:id
 =========================================================
 */
 
 
-export async function getShows(filters = {}) {
-  const response = await api.get("/shows", {
-    params: filters,
+export async function getShowById(showId) {
+  const response = await api.get(
+    `/shows/${showId}`,
+  );
+
+
+  return response.data;
+}
+
+/*
+=========================================================
+CREATE SHOW
+
+
+POST /api/shows
+=========================================================
+*/
+
+
+export async function createShow(showData) {
+  const token = localStorage.getItem("token");
+
+
+  const response = await api.post("/shows", showData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+
+  return response.data;
+}
+
+
+/*
+=========================================================
+UPDATE SHOW
+
+
+PUT /api/shows/:id
+=========================================================
+*/
+
+
+export async function updateShow(showId, showData) {
+  const token = localStorage.getItem("token");
+
+
+  const response = await api.put(`/shows/${showId}`, showData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+
+  return response.data;
+}
+
+
+/*
+=========================================================
+DELETE SHOW
+
+
+DELETE /api/shows/:id
+=========================================================
+*/
+
+
+export async function deleteShow(showId) {
+  const token = localStorage.getItem("token");
+
+
+  const response = await api.delete(`/shows/${showId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 
 
