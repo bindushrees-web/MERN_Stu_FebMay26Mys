dd578e02983297258a16544b28cde7eadb32a2c8
bindushrees-web <bindushrees754@gmail.com>
2026-07-09 08:04:54 +0530

    deleted files

--- Team Project/CLI-Based Professional Networking Platform/MERN_Week2_V1.0/lms-backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, logout } = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/login', login);
-router.post('/logout', authMiddleware, logout);
-
-module.exports = router;
